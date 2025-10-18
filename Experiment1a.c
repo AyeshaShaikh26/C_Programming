5698862fcb3c1b6373b5111a4e1ccf1f56b6a71d
AyeshaShaikh26 <ayeshaikh26@gmail.com>
2025-10-18 19:02:09 +0530

    Experiment1a.c  with no error
    Experiment1a I learned about how to use printf and escape sequence

---
+++ Experiment1a.c
@@ -0,0 +1,18 @@
+/*
+Name : Ayesha Ilias Shaikh 
+UIN : 251M024
+Class : Mechanical
+Div : F 
+*/
+#include <stdio.h>
+
+int main()
+{
+ printf("\nName\t: Ayesha Ilias Shaikh");
+ printf("\nAge\t: 18");
+ printf("\nClass\t: Mechanical");
+ printf("\nDivision: F");
+ printf("\nUIN\t: 251M024");
+
+ return 0;
+} 
