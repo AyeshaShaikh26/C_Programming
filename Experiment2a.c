e87bbdfb7ca7a44c3427d78baae32090bf800476
AyeshaShaikh26 <ayeshaikh26@gmail.com>
2025-10-18 19:17:54 +0530

    Program to check if a number is even or odd
    Experiment2a To determine If the number is Even or Odd

---
+++ Experiment2a.c
@@ -0,0 +1,24 @@
+/*
+NAME : Ayesha Ilias Shaikh
+UIN: 251M024
+Class: Mechanical
+Div: F
+*/
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+    printf("\nEnter Number : ");
+    scanf("%d",&num);
+    
+    if(num%2==0)
+    {
+        printf("\n%d is a Even Number",num);
+    }
+    else
+    {
+        printf("\n%d is a Odd Number",num);
+    }
+    return 0;
+}
